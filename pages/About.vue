ce11b3705911143c79015191a3def359a91f6f8c
Dave <dc@goodmonday.io>
2020-01-23 13:43:57 +0100

    Remove test component completely

--- pages/About.vue
+++ pages/About.vue
@@ -1,12 +1,16 @@
 <template>
     <div class="links">
-        <test></test>
+        <nuxt-link class="nav__link" to="/about/vision/">Our vision</nuxt-link>
     </div>
 </template>
 
 <script>
+import Test from "../components/test";
 
 export default {
+    components: {
+        Test
+    }
 }
 </script>
 
